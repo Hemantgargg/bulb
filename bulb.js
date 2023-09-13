7bb50816127ab9c0c47f31f9e41b5105c54b9d21
hemant garg <gargyhemant1@gmail.com>
2023-09-13 23:54:29 +0530

    bulb

---
+++ bulb.js
@@ -0,0 +1,27 @@
+//getting element from html
+let sw = document.getElementById("off")
+let bg = document.getElementById("con")
+
+//creating switch
+let state = false
+
+sw.addEventListener("click", toggleBulb);
+
+
+function toggleBulb() {
+    state = !state;
+
+    if (state) {
+        sw.style.background = "yellow"
+    }
+    else {
+        sw.style.background = "white"
+        bg.style.background = "green"
+
+
+    }
+}
+
+
+
+
